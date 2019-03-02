9aebaef3be47d2a386acb9abbbf3c131e8fc97fb
Peter Hurt <peter.hurt@sky.com>
2019-03-02 16:34:02 +0000

    Attempt to capture mouse
    In Neutro.java

--- src/neutro/Main.java
+++ src/neutro/Main.java
@@ -6,12 +6,10 @@ import atlas.engine.Window;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Window window = new Window("Space Man",1280,720,true);
 		Neutro game = new Neutro();
 		//Game, Window, Fixed Updates, Max Frame Rate
 		Engine engine = new Engine(game, window, 20, 120);
 		engine.start();
 	}
-
 }
